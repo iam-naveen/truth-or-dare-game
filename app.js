8222b192ff570d0772a69d4d830884674ad070e5
Naveen <87268854+iam-naveen@users.noreply.github.com>
2022-08-09 22:32:51 +0530

    v1 commit

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
 const app = express();
+app.use(express.static("static"));
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-  res.send("hello");
+  res.sendFile(__dirname + "/game.html");
+  res.render("question", { OUTPUT: "" });
+});
+
+app.post("/", function (req, res) {
+  console.log(req.body);
+  const type = req.body.type;
+  const rating = req.body.rating;
+  const url =
+    "https://api.truthordarebot.xyz/api/" + type + "?rating=" + rating;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => res.render("question", { OUTPUT: data.question }));
 });
 
 app.listen(process.env.PORT || 3000, function () {
